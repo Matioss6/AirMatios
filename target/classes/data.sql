5562f1502473cd67539739d5637228bd8438fba5
Matios <matioss6@gmail.com>
2022-11-03 17:44:40 +0100

    Login / Getting users data - done

---
+++ target/classes/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO User_Data VALUES ('Matios', '123', 'Mateusz', 'Pacholski');
+INSERT INTO User_Data VALUES ('aaaaa', '222', 'abcd', 'abab');
+INSERT INTO User_Data VALUES ('bbbbb', '333', 'aabc', 'aabc');
+INSERT INTO User_Data VALUES ('ccccc', '444', 'cdse', 'ccdde');
+INSERT INTO User_Data VALUES ('ddddd', '555', 'frre', 'jfftyg');
